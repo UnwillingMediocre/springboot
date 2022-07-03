254db6943240a6cc19ef5f50d8c8445e44d985bf
kimyuan <kimyuan@rektec.com.cn>
2022-07-03 16:41:57 +0800

    修改helloworld的package

--- demo/demo-helloworld/src/main/java/com/stodger/springboot/helloworld/HelloWorldApplication.java
+++ demo/demo-helloworld/src/main/java/com/stodger/springboot/helloworld/HelloWorldApplication.java
@@ -1,4 +1,4 @@
-package com.stodger.springboot;
+package com.stodger.springboot.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
