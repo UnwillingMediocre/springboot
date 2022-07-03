82a48811942a4a4b330d016a60f7b81dc0637d77
kimyuan <kimyuan@rektec.com.cn>
2022-07-03 20:31:29 +0800

    修改logback内容

--- demo/demo-logback/src/test/java/com/stodger/springboot/logback/LogbackTest.java
+++ demo/demo-logback/src/test/java/com/stodger/springboot/logback/LogbackTest.java
@@ -3,6 +3,7 @@ package com.stodger.springboot.logback;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.MDC;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,6 +20,7 @@ public class LogbackTest {
 
     @Test
     public void  logTest(){
+        MDC.put("userId","stodger");
         for(int i = 0; i < 100; i++) {
             log.info("test info");
             log.error("test error");
